088da0dfa8c7cbaaf90b1e175f7f5efabc3248c1
Francesco Maura <francesco92.maura@gmail.com>
2024-05-10 19:37:54 +0200

    css selector

--- Notes_WebScraping.R
+++ Notes_WebScraping.R
@@ -1,3 +1,21 @@
 ###### WebScraping ###### 
 # McDermott slide pack: 06_web_css.pdf
 
+# Introductory Notes:
+# I’ll be using SelectorGadget, which is a Chrome extension that makes it easy to discover CSS selectors. (Install the
+# extension directly here.)
+
+## Install development version of rvest if necessary
+if (numeric_version(packageVersion("rvest")) < numeric_version('0.99.0')) {
+  remotes::install_github('tidyverse/rvest')
+}
+## Load and install the packages that we'll be using today
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, rvest, lubridate, janitor, data.table, hrbrthemes)
+## My preferred ggplot2 plotting theme (optional)
+theme_set(hrbrthemes::theme_ipsum())
+
+# rvest is designed to work with webpages that are built server-side and
+# thus requires knowledge of the relevant CSS selectors
+
+# What is a CSS (Cascading Style Sheets) selector? ###############

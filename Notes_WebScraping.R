cdcef4e0eaf9def0b0380d86c6826b0fb6da3dd1
Francesco Maura <francesco92.maura@gmail.com>
2024-05-13 08:47:29 +0200

    CSS summary and first wikipedia page scraping

--- Notes_WebScraping.R
+++ Notes_WebScraping.R
@@ -19,3 +19,35 @@ theme_set(hrbrthemes::theme_ipsum())
 # thus requires knowledge of the relevant CSS selectors
 
 # What is a CSS (Cascading Style Sheets) selector? ###############
+
+# The CSS is a language that gives HTML files (e.g., webpages) their appearence.
+# It does so assigning 2 charachteristics:
+# PROPERTIES: is the "how I want things to appear"? (like fonts, colours...)
+# SELECTORS: is the "what do I want to appear?". It is like a tag attached to
+#            elements. ".h1" stands for header1, which might be the titlepage
+#            Thus, ".h1" elements have specific fonts, bold, colours...
+
+# An Example: WikiPedia 100m Males WR
+library(rvest)
+m100 <- read_html("http://en.wikipedia.org/wiki/Men%27s_100_metres_world_record_progression")
+m100
+# that's a XML document (Extensible Markup Language) like viewing all the LaTeX 
+# document of a paper, while we only want some tables.
+
+# Using SelectorGadget, we can find the CSS of, e.g., the first table of the page
+# that's the corresponding CSS "div+ .wikitable :nth-child(1)"
+# from rvest, we use html_element and html_table (for conversion into df)
+pre_iaaf <-
+  m100 %>%
+  html_element("div+ .wikitable :nth-child(1)") %>% ## select table element
+  html_table()                                      ## convert the table into a df
+pre_iaaf
+
+# clean thing a bit
+library(janitor) ## Already loaded
+library(lubridate) ## Already loaded
+pre_iaaf <-
+  pre_iaaf %>%
+  clean_names() %>% ## fix the column names
+  mutate(date = mdy(date)) ## convert string to date format
+pre_iaaf

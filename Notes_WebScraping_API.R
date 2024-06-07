611d28bf7cbc7fb4ad1cfc5ea40b4dcbba5aebb6
Francesco Maura <francesco92.maura@gmail.com>
2024-06-07 17:50:39 +0200

    1st example API data dowload: NYC trees

--- Notes_WebScraping_API.R
+++ Notes_WebScraping_API.R
@@ -36,3 +36,34 @@ lapply(some_packages, library, character.only=TRUE)
 
 # Application 1: Trees of New York City
 
+# we’re going to do something “earthy” for this first application: 
+# Download a sample of tree data from the 2015 NYC Street Tree Census.
+
+# • Open the web page in your browser (if you haven’t already done so):
+# https://data.cityofnewyork.us/Environment/2015-Street-Tree-Census-Tree-Data/uvpi-gqnh/about_data
+# • You should immediately see the API tab. Click on it.
+# • Copy the API endpoint that appears in the popup box.
+# • Optional: Paste that endpoint into a new tab in your browser. You’ll see a bunch
+
+# Now that we’ve located the API endpoint, let’s read the data into R. We’ll do so using the fromJSON() function
+# library(jsonlite) ## Already loaded
+nyc_trees <- fromJSON("https://data.cityofnewyork.us/resource/uvpi-gqnh.json") %>%
+  as_tibble()
+nyc_trees
+
+# to read in only the first five rows, you could use:
+## (Not run)
+fromJSON("https://data.cityofnewyork.us/resource/uvpi-gqnh.json?$limit=5")
+
+# plot (just to see that what we have done is working and not useless)
+nyc_trees %>%
+  select(longitude, latitude, stump_diam, spc_common, spc_latin, tree_id) %>%
+  mutate_at(vars(longitude:stump_diam), as.numeric) %>%
+  ggplot(aes(x=longitude, y=latitude, size=stump_diam)) +
+  geom_point(alpha=0.5) +
+  scale_size_continuous(name = "Stump diameter") +
+  labs(
+    x = "Longitude", y = "Latitude",
+    title = "Sample of New York City trees",
+    caption = "Source: NYC Open Data"
+  )
